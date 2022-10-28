6df44b71a7514e39cee570616101126316dc375e
THE-FABI7 <fabianguancha13@gmail.com>
2022-10-27 20:08:57 -0500

    first commit

---
+++ src/main/java/com/example/platziday/PlatzidayApplication.java
@@ -0,0 +1,13 @@
+package com.example.platziday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatzidayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatzidayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/platziday/PlatzidayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.platziday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatzidayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
